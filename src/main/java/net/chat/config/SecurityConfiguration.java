98f0f9fc94878d0aa0162a219702f1d3b36207ab
Mariusz Gorzycki <ziper22@gmail.com>
2016-04-13 23:44:19 +0200

    R Deploy security

--- src/main/java/net/chat/config/SecurityConfiguration.java
+++ src/main/java/net/chat/config/SecurityConfiguration.java
@@ -1,7 +1,7 @@
 package net.chat.config;
 
 import net.chat.config.authentication.AuthenticationFilter;
-import net.chat.config.authentication.PasswordAuthenticationProvider;
+import net.chat.config.authentication.TokenAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,11 +22,11 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    PasswordAuthenticationProvider passwordAuthenticationProvider;
-
 //    @Autowired
-//    TokenAuthenticationProvider tokenAuthenticationProvider;
+//    PasswordAuthenticationProvider passwordAuthenticationProvider;
+
+    @Autowired
+    TokenAuthenticationProvider tokenAuthenticationProvider;
 
     @Bean
     @Override
@@ -40,8 +40,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("t").password("t").roles("USER").and()
                 .withUser("a").password("a").roles("USER", "ADMIN");
 
-        auth.authenticationProvider(passwordAuthenticationProvider);
-//        auth.authenticationProvider(tokenAuthenticationProvider);
+//        auth.authenticationProvider(passwordAuthenticationProvider);
+        auth.authenticationProvider(tokenAuthenticationProvider);
     }
 
     @Override
