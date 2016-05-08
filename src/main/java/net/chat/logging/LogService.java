ee692731fdb83918a92ccd294b2990750197791a
Mariusz Gorzycki <ziper22@gmail.com>
2016-05-08 21:00:25 +0200

    Deploy

--- src/main/java/net/chat/logging/LogService.java
+++ src/main/java/net/chat/logging/LogService.java
@@ -62,11 +62,34 @@ public class LogService {
             remoteAddress += ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getRemoteHost() + " : ";
             remoteAddress += ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getRemoteAddr() + " : ";
             remoteAddress += ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getRemotePort();
+            remoteAddress = getClientIpAddr(request);
+            HttpServletRequest request2 = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+            request2.toString();
         } catch (Exception e) {
 
         }
 
         return remoteAddress;
+    }
+
+    public static String getClientIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 
     private void dispatchLog(LogEntity logEntity) {
